16fb3db6db136547439f503377f3da20136d58ff
Arlette Espinal <arletteespinal00@gmail.com>
2016-02-01 14:34:09 -0400

    Commit
    commit: clase _get_datatables_query hecha

---
+++ application/models/person_model.php
@@ -0,0 +1,44 @@
+<?php
+	defined('BASEPATH') or exit('No direct script access allowed');
+
+	/**
+	* 
+	*/
+	class Person_model extends CI_Model{
+		
+		function __construct(){
+			parent::__construct();
+			$this->load->database();
+		}
+
+		private function _get_datatables_query(){
+			$this->db->from($this->table);
+			$i=0;
+
+			foreach ($this->column as $item) {
+				if($_POST['search']['value']){
+					if($i===0){
+						$this->db->group_start();
+						$this->db->like($item,$_POST['search']['value']);
+					}else{
+						$this->db->or_like($item,$_POST['search']['value']);
+					}
+
+					if(count($this->column)-1==$i)
+						$this->db->group_end();
+				}
+
+				$column[$i] = $item;
+				$i++;
+			}
+
+			if(isset($_POST['order'])){
+				$this->db->order_by(key($order),$order[key($order)]);
+			}
+			elseif (isset($this->order)) {
+				$order = $this->order;
+				$this->db->order_by(key($order),$order[key($order)]);
+			}
+		}
+	}
+?>
