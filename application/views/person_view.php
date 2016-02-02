9a2d6360809f916075b7ec0fe89405378ad93e1c
Arlette Espinal <arletteespinal00@gmail.com>
2016-02-02 09:18:12 -0400

    terminando div container

--- application/views/person_view.php
+++ application/views/person_view.php
@@ -21,7 +21,35 @@
 		<h3>Person Data</h3>
 		<br/>
 		<button class="btn btn-success" onclick="add_person()"><i class="glyphicon glyphicon-plus"></i> Add Person</button>
-		<button></button>
+		<button class="btn btn-default" onclick="reload_table()"> <i class="glyphicon glyphicon-refresh"></i>Reload</button>
+        <br/>
+        <br/>
+        <table id="table" class="table table-striped table-bordered" cellspacing="0" width="100%">
+                 <thead>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Gender</th>
+                    <th>Address</th>
+                    <th>Date of Birth</th>
+                    <th style="width:125px;">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+ 
+            <tfoot>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Gender</th>
+                <th>Address</th>
+                <th>Date of Birth</th>
+                <th>Action</th>
+            </tr>
+            </tfoot>
+
+        </table>
 
 
 	</div>
